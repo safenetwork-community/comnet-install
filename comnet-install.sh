7f08cad93e359e6ad4ebd4b68ad2401ae08250ec
Southside <293741+willief@users.noreply.github.com>
2022-01-30 08:24:37 +0000

    Added folaht config   vault size and user confirm

--- comnet-install.sh
+++ comnet-install.sh
@@ -7,22 +7,41 @@ echo ""
 echo " any existing SAFE installation will be DELETED"
 echo " vdash is a program by @happybeing to monitor your SAFE node."
 echo " Rust will be installed if necessary"
-echo "OK to proceed (Y/n)"
-read 
+echo ""
+echo "OK to proceed [Y,n]"
+read input
+
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "OK then..."
+else
+       echo "Bye now..."
+       
+       exit
+fi
+
+
+read -p " Enter the vault size in Gb [ default 5Gb]: " VAULT_SIZE
+VAULT_SIZE=${VAULT_SIZE}
+echo $VAULT_SIZE "Gb will be allocated for storing chunks"
+
 
 sudo apt update
 sudo apt install snapd build-essential
 sudo snap install curl
 
-
+#exit 
 
 LOCAL_IP=$(echo `ifdata -pa enp5s0`)
 PUBLIC_IP=$(echo `curl -s ifconfig.me`)
 SAFE_PORT=12000
-SAFENET=comnet
-CONFIG_URL=https://sn-comnet.s3.eu-west-2.amazonaws.com/node_connection_info.config
+SAFENET=folaht
+CONFIG_URL=https://link.tardigradeshare.io/s/julx763rsy2egbnj2nixoahpobgq/rezosur/koqfig/sjefolaht_node_connection_info.config?wrap=0
+#CONFIG_URL=https://sn-comnet.s3.eu-west-2.amazonaws.com/node_connection_info.config
 MAX_NODE_CAPACITY=$(numfmt --from auto 5Gi)
 LOG_DIR=$HOME/.safe/node/local_node
+
+
+
 
 
 # Install Safe software and configuration
@@ -56,7 +75,7 @@ echo "--local-addr" $LOCAL_IP":"$SAFE_PORT
 echo $LOCAL_IP
 
 
-RUST_LOG=safe_network=trace,qp2p=info \
+RUST_LOG=safe_network=trace \
     ~/.safe/node/sn_node \
     --max-capacity $MAX_NODE_CAPACITY \
     --local-addr $LOCAL_IP:$SAFE_PORT \
@@ -83,18 +102,6 @@ sudo snap install rustup --classic
 rustup toolchain install stable
 cargo install vdash
 vdash $LOG_DIR/sn_node.log
-
-
-# get sn_cli
-cd
-curl -so- https://install-safe.s3.eu-west-2.amazonaws.com/install.sh | bash
-safe node install
-
-# set up the network
-safe networks add comnet $COMNET_CONN_INFO
-safe networks switch comnet
-safe networks check
-safe networks
 
 
 echo "-----------------------------   Getting Started   -----------------------------------------------------------"
